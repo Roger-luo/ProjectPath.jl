036fcf2ff4477f095c54fcc9ad7122d534fb3336
Roger-luo <rogerluo.rl18@gmail.com>
2020-09-05 17:38:00 -0400

    mv from Comonicon

--- src/ProjectPath.jl
+++ src/ProjectPath.jl
@@ -1,5 +1,23 @@
 module ProjectPath
 
-# Write your package code here.
+using Pkg
+
+function project(m::Module, xs...)
+    path = pathof(m)
+    path === nothing && return dirname(Pkg.project().path)
+    return normpath(joinpath(dirname(dirname(path)), xs...))
+end
+
+src(m::Module, xs...) = project(m, "src", xs...)
+deps(m::Module, xs...) = project(m, "deps", xs...)
+test(m::Module, xs...) = project(m, "test", xs...)
+
+macro include(xs...)
+    return esc(:(Base.include($project($__module__, "src", $(xs...)))))
+end
+
+macro project(xs...)
+    return esc(:($project($__module__, $(xs...))))
+end
 
 end
